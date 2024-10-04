2b825ab4755009b50ec48d22745003ff05430028
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-10-04 21:10:25 +0100

    Memory Game

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import Card from './components/Card'
 function App() {
   const [score, setScore] = useState(0);
   const [character, setCharacter] = ('');
-  
+  const characterNames = ['Sulaiman', 'Aliyu', 'Muhammad']
   function onClickCard () {
     setScore(score + 1)
   }
   useEffect(() => {
-    
+
   },[character])
 
   return (
     <>
       <Navbar bestScore={score} currentScore={7} />
       <div className='w-2/3 my-0 mx-auto flex justify-between'>
-        <Card characterName={'Sulaiman'} updateNo={onClickCard} />
+        {/* <Card characterName={'Sulaiman'} updateNo={onClickCard} />
         <Card characterName={'Isa'} updateNo={() => setScore(score + 1)}/>
-        <Card characterName={'Muhammad'} updateNo={() => setScore(score + 1)}/>
+        <Card characterName={'Muhammad'} updateNo={() => setScore(score + 1)}/> */}
+        
       </div>
       
     </>
